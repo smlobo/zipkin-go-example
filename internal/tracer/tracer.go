cd74a6f9409a28e15c0ec458d01dd65933f2f12a
Sheldon Lobo <slobo@aternity.com>
2021-04-29 22:57:50 -0400

    * Add the preferred local machine IPv4 address to the LocalEndPoint. * Add copyright.

--- internal/tracer/tracer.go
+++ internal/tracer/tracer.go
@@ -1,3 +1,17 @@
+// Copyright 2021 Sheldon Lobo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tracer
 
 import (
@@ -7,6 +21,8 @@ import (
 	"github.com/openzipkin/zipkin-go/reporter"
 	httpreporter "github.com/openzipkin/zipkin-go/reporter/http"
 	exampleconfig "github.com/smlobo/zipkin-go-example/internal/config"
+	"log"
+	"net"
 )
 
 func NewTracer(serviceName string, port uint16) (*zipkin.Tracer, error) {
@@ -23,7 +39,7 @@ func NewTracer(serviceName string, port uint16) (*zipkin.Tracer, error) {
 	}
 
 	// Local endpoint represent the local service information
-	localEndpoint := &model.Endpoint{ServiceName: serviceName, Port: port}
+	localEndpoint := &model.Endpoint{ServiceName: serviceName, IPv4: getOutboundIP(), Port: port}
 
 	// Sampler tells you which traces are going to be sampled or not. In this case we will record 100% (1.00)
 	// of traces.
@@ -42,4 +58,17 @@ func NewTracer(serviceName string, port uint16) (*zipkin.Tracer, error) {
 	}
 
 	return tracer, err
+}
+
+// Get preferred outbound ip of this machine
+func getOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
